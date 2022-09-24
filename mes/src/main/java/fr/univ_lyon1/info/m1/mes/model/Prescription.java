cc7297ea50e288ab26e224d7d921b5215452b8c7
romeo <romeo.phang.7@outlook.fr>
2022-09-24 14:56:11 +0200

    enhanced Prescription model annotations

--- mes/src/main/java/fr/univ_lyon1/info/m1/mes/model/Prescription.java
+++ mes/src/main/java/fr/univ_lyon1/info/m1/mes/model/Prescription.java
@@ -2,14 +2,14 @@ package fr.univ_lyon1.info.m1.mes.model;
 
 public class Prescription {
 
-    /*
-     * Attributes
+    /**
+     * Attributes.
      */
     private final HealthProfessional hp;
     private final String content;
 
-    /*
-     * Constructor
+    /**
+     * Prescription constructor.
      * 
      * @param hp
      * @param content
@@ -20,6 +20,8 @@ public class Prescription {
     }
 
     /**
+     * Return prescription content.
+     * 
      * @return String
      */
     public String getContent() {
@@ -27,6 +29,8 @@ public class Prescription {
     }
 
     /**
+     * Return the HealthProfessional.
+     * 
      * @return HealthProfessional
      */
     public HealthProfessional getHealthProfessional() {

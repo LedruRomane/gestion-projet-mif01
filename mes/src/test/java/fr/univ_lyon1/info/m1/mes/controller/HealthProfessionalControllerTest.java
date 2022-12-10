9e9ea570f8b37266fe336df67f182babf258cc19
rphang <root@localhost>
2022-12-10 17:42:07 +0100

    Controller tests

---
+++ mes/src/test/java/fr/univ_lyon1/info/m1/mes/controller/HealthProfessionalControllerTest.java
@@ -0,0 +1,146 @@
+package fr.univ_lyon1.info.m1.mes.controller;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javafx.application.Platform;
+import fr.univ_lyon1.info.m1.mes.model.HealthProfessional;
+import fr.univ_lyon1.info.m1.mes.model.HealthProfessionalFactory;
+import fr.univ_lyon1.info.m1.mes.model.MES;
+import fr.univ_lyon1.info.m1.mes.model.Patient;
+import fr.univ_lyon1.info.m1.mes.model.Prescription;
+import fr.univ_lyon1.info.m1.mes.types.HealthProfessionalType;
+import fr.univ_lyon1.info.m1.mes.types.PatientSearchStrategyType;
+
+public class HealthProfessionalControllerTest {
+    @BeforeAll
+    public static void setUp() {
+        Platform.startup(() -> {});
+    }
+
+    @Test
+    public void instantiateHealthProfessionalController() throws Exception {
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+        assertThat(healthProfessionalController, is(healthProfessionalController));
+    }
+
+    @Test
+    public void deletePrescriptionWithoutSelect() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+
+        Patient patient = new Patient("testProControllerPatient1", "4111");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "testProControllerHP1");
+        Prescription p = new Prescription(d, "testProControllerPrescription1");
+
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+        patient.addPrescription(p);
+
+        // When
+        try {
+            healthProfessionalController.deletePrescription(p);
+        } catch (Error e) {
+           // Then
+            assertThat(e.getMessage(), is("Missing selected patient"));
+        }
+    }
+
+    @Test
+    public void addPrescriptionWithoutSelect() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+
+        Patient patient = new Patient("testProControllerPatient1", "4111");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "testProControllerHP1");
+
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+
+        // When
+        try {
+            healthProfessionalController.addPrescription("testPresc");
+        } catch (Error e) {
+           // Then
+            assertThat(e.getMessage(), is("Missing selected patient"));
+        }
+    }
+
+    @Test
+    public void selectAndDeletePrescriptionHealthProfessional() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+
+        Patient patient = new Patient("testProControllerPatient1", "4111");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "testProControllerHP1");
+        Prescription p = new Prescription(d, "testProControllerPrescription1");
+
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+        patient.addPrescription(p);
+
+        // When
+        healthProfessionalController.selectHealthProfessional(d);
+        healthProfessionalController.selectPatient(patient);
+        healthProfessionalController.deletePrescription(p);
+        // Then
+        assertThat(patient.getPrescriptions().size(), is(0));
+    }
+
+    @Test
+    public void selectAndAddPrescriptionHealthProfessional() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+
+        Patient patient = new Patient("testProControllerPatient2", "4112");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "testProControllerHP2");
+
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+
+        // When
+        healthProfessionalController.selectHealthProfessional(d);
+        healthProfessionalController.selectPatient(patient);
+        healthProfessionalController.addPrescription("testProControllerPrescription2");
+        // Then
+        assertThat(patient.getPrescriptions().size(), is(1));
+        assertThat(patient.getPrescriptions().get(0).getHealthProfessional(), is(d));
+    }
+
+    @Test
+    public void getPatients() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+
+        Patient patient = new Patient("testProControllerPatient3", "4113");
+
+        // When
+        model.addPatient(patient);
+
+        // Then
+        assertThat(healthProfessionalController.getPatients(PatientSearchStrategyType.NAME, "testProControllerPatient3").size(), is(1));
+    }
+
+    @Test
+    public void createHealthProfessional() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        HealthProfessionalController healthProfessionalController = new HealthProfessionalController(model);
+        Integer size = model.getHealthProfessionals().size();
+        // When
+        healthProfessionalController.createHealthProfessional(HealthProfessionalType.DENTIST, "testProControllerHP3");
+
+        // Then
+        assertThat(model.getHealthProfessionals().size(), is(size + 1));
+    }
+}
---
+++ mes/src/test/java/fr/univ_lyon1/info/m1/mes/controller/PatientControllerTest.java
@@ -0,0 +1,104 @@
+package fr.univ_lyon1.info.m1.mes.controller;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javafx.application.Platform;
+import fr.univ_lyon1.info.m1.mes.model.HealthProfessional;
+import fr.univ_lyon1.info.m1.mes.model.HealthProfessionalFactory;
+import fr.univ_lyon1.info.m1.mes.model.MES;
+import fr.univ_lyon1.info.m1.mes.model.Patient;
+import fr.univ_lyon1.info.m1.mes.model.Prescription;
+import fr.univ_lyon1.info.m1.mes.types.HealthProfessionalType;
+
+public class PatientControllerTest {
+
+    @Test
+    public void instantiatePatientController() throws Exception {
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        assertThat(patientController, is(patientController));
+    }
+
+    @Test
+    public void selectAndDeletePrescriptionPatient() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        Patient patient = new Patient("test", "1234");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "test");
+        Prescription prescription = new Prescription(d, "test");
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+        patient.addPrescription(prescription);
+        // When
+        patientController.selectPatient(patient);
+        patientController.deletePrescription(prescription);
+        // Then
+        assertThat(patient.getPrescriptions().size(), is(0));
+    }
+
+    @Test
+    public void selectAndDeletePrescriptionNullPatient() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        Patient patient = new Patient("test", "1234");
+        HealthProfessional d = HealthProfessionalFactory.createHealthProfessional(HealthProfessionalType.DENTIST, "test");
+        Prescription prescription = new Prescription(d, "test");
+        model.addHealthProfessional(d);
+        model.addPatient(patient);
+        patient.addPrescription(prescription);
+        // When
+        patientController.selectPatient(null);
+        try {
+            patientController.deletePrescription(prescription);
+        } catch (NullPointerException e) {
+            // Then
+            assertThat(patient.getPrescriptions().size(), is(1));
+            assertThat(patient.getPrescriptions().get(0), is(prescription));
+        }
+    }
+
+    @Test
+    public void patientCreation() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        Integer size = model.getPatients().size();
+        // When
+        patientController.createPatient("patientTestCreation", "2222");
+        // Then
+        assertThat(model.getPatients().size(), is(size + 1));
+        assertThat(model.getPatients().get(size).getName(), is("patientTestCreation"));
+        assertThat(model.getPatients().get(size).getSsid(), is("2222"));
+    }
+
+    @Test
+    public void patientDuplicateCreation() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        Integer size = model.getPatients().size();
+        // When
+        patientController.createPatient("patientTestCreationDuplicate", "3333");
+        patientController.createPatient("patientTestCreationDuplicate", "3333");
+        
+        // Then
+        assertThat(model.getPatients().size(), is(size + 1));
+
+    }
+
+    @Test
+    public void getViewTest() throws Exception {
+        // Given
+        MES model = MES.getInstance();
+        PatientController patientController = new PatientController(model);
+        // Then
+        assertThat(patientController.getView(), is(patientController.getView()));
+    }
+}
